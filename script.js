8203219757e2dc6c57b717bc5607bc718688d12a
Filip Vrubel <114007566+FilipVrubel@users.noreply.github.com>
2024-10-16 18:55:23 +0200

    Implement displaying books

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ function addBookToLibrary() {
     const book = new Book(name);
     myLibrary.push(book);
 }
+
+/*
+    1. remove any existent cards
+    2. loop through book array, create card for each book and append it into container
+
+*/
+
+function displayBooks() {
+
+    const container = document.querySelector(".card-container");
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const card = document.querySelector(".card");
+        
+        if (card != null) {
+            container.removeChild(card)
+        }
+    }
+    
+    for (const book of myLibrary) {
+        const card = document.createElement("div");
+        card.classList.add("card")
+        card.textContent = book.name;
+        container.appendChild(card);
+    }
+}
+
